9fea5d245e8e25954f5a1b7690fa085b4463836d
aaaaaaaaak <ahc1079@gmail.com>
2020-07-15 03:45:34 +0900

    Add example to yacht_dice_ui.py

---
+++ yacht_dice_ui.py
@@ -0,0 +1,9 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Yacht Dice")
+window.geometry("1280x720+100+100")
+window.configure(background="black")
+window.resizable(False, False)
+
+window.mainloop()
